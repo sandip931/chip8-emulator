8101d0083d2c30889460b7062456b3e92fa3a3f1
sandip <sp7354444@gmail.com>
2026-05-27 12:44:42 +0545

    CPU structure

--- includes/chip8.h
+++ includes/chip8.h
@@ -8,22 +8,23 @@ class chip8{
   private: 
     // uint8_t memory[RAM_SIZE];
   public:
-    uint8_t V[16];
-    uint16_t I;
-
+    ///  ------------------- CPU
+    uint8_t V[16]; /// register v0-vf
+    uint16_t I; // index regester
+    uint16_t pc; // program counter 
     uint16_t stack[16];
-    uint8_t sp;
+    uint8_t sp; // stack pointer
 
+    //TIMERS
     uint8_t delayTimer;
-    uint8_t soundTimer;
+    uint8_t soundTimer; 
 
-    uint8_t display[64 * 32];
-
-    bool keys[16];
-
+    int display[64 * 32]; // display frame 
+    
+    int keypad[16];
+  
     bool drawFlag;
 
-    uint16_t pc;
 
 
 };
