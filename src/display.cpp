4188fc005898ce46a62f885ceee7d024a4b4dbec
D3190 <aadarsha.love@gmail.com>
2026-05-28 23:40:13 +0545

    used renderer instead of surface as renderer is used for display

--- src/display.cpp
+++ src/display.cpp
@@ -3,13 +3,13 @@
 
 int main(int argc, char **args)
 {
-
-    SDL_Surface *winSurface = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *texture = NULL;
     SDL_Window *window = NULL;
     // checking for initilizing SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        std::cout << "Error when initializing SDL";
+        std::cout << "Error when initializing SDL" << SDL_GetError();
         system("pause");
         return 1;
     }
@@ -23,20 +23,22 @@ int main(int argc, char **args)
         return 1;
     }
 
-    winSurface = SDL_GetWindowSurface(window);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    if (!winSurface)
+    if (!renderer)
     {
-        std::cout << "Error getting surface" << SDL_GetError() << std::endl;
+        std::cout << "Error rendering" << SDL_GetError() << std::endl;
         system("pause");
         return 1;
     }
+    SDL_Rect pixel = {20, 20, 100, 200};
+    SDL_RenderClear(renderer);
 
-    SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 255, 255));
+    SDL_SetRenderDrawColor(renderer, 255, 128, 255, 255);
+    SDL_RenderFillRect(renderer, &pixel);
 
-    SDL_UpdateWindowSurface(window);
+    SDL_RenderPresent(renderer);
     system("pause");
-
     SDL_DestroyWindow(window);
 
     SDL_Quit();
